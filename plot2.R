d7f8be58fe43a950befbab42033c94143d594f1b
smcd8 <smcd0000@gmail.com>
2016-04-20 20:44:57 -0600

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,10 @@
+## Plot 2
+
+plot.new()
+frame()
+
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
